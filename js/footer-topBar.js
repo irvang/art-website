a7f7b03ae2fc0779727b666f52e7dc4d3632e571
Irving Angulo <irvang.manage@gmail.com>
2018-03-28 11:38:36 -0400

    Changes on Footer and Nav.
    -Changed footer to responsive with flexbox.
    -In footer, <a> no longer inside spans, they are childs of footer, and they are flex items.
    -Background-color on nav's hover is now dodgerblue.
    -Changed className undecorate to redecorate (css line 140).
    
    IMPORTANT NOTE: Some numbers may have changed due to some editing, but they are pretty close.
    (format-relation: cssLine, to htmlLine | cssLine, to htmlLine) 
    
    background-color: 16,15 | 20,46 | 82,27 | 87,27 | 92,54 | 146,120
    
    color: 28,17 | 83,28 | 97,55 | 147,120 | 153,108
    
    border: unused, present for testing purposes: 13 | 38 | 55 | 63 | 67 | 72 | 77 | 117 
    
    font-size: 2, 2 & 15 | 33,19 | 98,55 | 143,119
    
    font-style: 34,19
    
    padding: 12,15 | 67,26 | 82,28 | 119, 58 & 59
    
    margin: 10 & 11,15 | 23,46 | 55,24 | 94,54 | 101 & 102,55 | 111,56 | 120,58 & 59 | 159,105
    
    Other properties used: position, text-align, font-size, display, list-style-type, flex-wrap, text-decoration, flex-flow, align-content, align-items, max-width, flex-direction

--- js/footer-topBar.js
+++ js/footer-topBar.js
@@ -3,16 +3,10 @@
 	let toTopP = document.querySelector(".toTop");
 
 	footer.innerHTML = `
-	<h1 class="invisibleHeader"> Follow us</h1>
-	<span>
-		<a href="" class="undecorate">GitHub</a>
-	</span>
-	<span>
-		<a href="" class="undecorate">CodePen</a>
-	</span>
-	<span>
-		<a href="" class="undecorate">Facebook</a>
-	</span>
+		<h1 class="invisibleHeader"> Follow us</h1>
+		<a href="" class="redecorate">GitHub</a>
+		<a href="" class="redecorate">CodePen</a>
+		<a href="" class="redecorate">Facebook</a>
 	`;
 	
 	toTopP.innerHTML = '<a href="#">Back to top</a>';
